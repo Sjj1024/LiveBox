b694186d7bffa22af408b15843b431256b050b5f
1024小神 <15670339118@163.com>
2024-06-30 22:54:31 +0800

    update

--- src-tauri/src/command/live.rs
+++ src-tauri/src/command/live.rs
@@ -11,7 +11,7 @@ pub async fn greet_you(name: &str) -> Result<String, String> {
 #[tauri::command]
 pub async fn get_live_html(url: &str) -> Result<LiveInfo, String> {
     // let response = reqwest::get(live_url).await.unwrap();
-    println!("调用了get_live_html");
+    // println!("调用了get_live_html");
     let mut live_req = DouYinReq::new(url);
     // 获取直播间room_id和主播信息
     let result = live_req.get_room_info().await;
